954e138d8a197f7d58c9c86812fdae4f53afa32c
RyanNavillus <ryanpnavillus@gmail.com>
2018-11-29 22:50:33 -0500

    Add genre model

--- model.py
+++ model.py
@@ -50,9 +50,9 @@ zipped = list(zip(X, y))
 random.shuffle(zipped)
 shuffled_X, shuffled_y = zip(*zipped)
 
-split = int(0.8 * len(shuffled_X))
-train_X, test_X = shuffled_X[:split], shuffled_X[split:]
-train_y, test_y = shuffled_y[:split], shuffled_y[split:]
+split = int(0.8 * len(X))
+train_X, test_X = X[:split], X[split:]
+train_y, test_y = y[:split], y[split:]
 
 model = linear_model.LinearRegression(fit_intercept=True)
 

0959c0989758a4de6c19ec6485501135a91a4c50
Max O'Cull <max.ocull@protonmail.com>
2018-11-30 20:32:51 -0500

    Begin adding genres to plots

--- genre.py
+++ genre.py
@@ -4,80 +4,84 @@ from sklearn import tree, metrics, preprocessing
 import pandas
 import random
 
-csv_file = open("data/dataset.csv", "r")
-
-data = pandas.read_csv("data/dataset.csv")
-print(data)
-X = data.loc[
-    :,
-    (
-        "runtimeMinutes",
-        "numVotes",
-        "directorsExperience",
-        "actorsExperience",
-        "averageRating"
-    ),
-]
-y = data.loc[
-    :, 
-    (
-        "isAdult",
-        "isDrama",
-        "isComedy",
-        "isShort",
-        "isDocumentary",
-        "isTalkShow",
-        "isRomance",
-        "isFamily",
-        "isNews",
-        "isAnimation",
-        "isRealityTV",
-        "isMusic",
-        "isCrime",
-        "isAdventure",
-        "isGameShow",
-        "isMystery",
-        "isSport",
-        "isFantasy",
-        "isHorror",
-        "isThriller",
-        "isSciFi",
-        "isHistory",
-        "isBiography",
-        "isMusical",
-        "isWestern",
-        "isWar",
-        "isFilmNoir",
-    )
-]
-zipped = list(zip(X, y))
-random.shuffle(zipped)
-shuffled_X, shuffled_y = zip(*zipped)
-
-split = int(0.8 * len(X))
-train_X, test_X = X[:split], X[split:]
-train_y, test_y = y[:split], y[split:]
-
-model = tree.DecisionTreeClassifier()
-
-model.fit(train_X, train_y)
-predictions = model.predict(test_X)
-correct = 0.0
-perfect = 0.0
-for i, prediction in enumerate(predictions):
-    differences = sum(abs(prediction - test_y.loc[i+split]))
-    score = (len(prediction)-differences)/len(prediction)
-    correct += score
-    print(i)
-    print("Predict:\t{}".format(list(prediction.astype(int))))
-    print("Labels: \t{}".format(list(test_y.loc[i+split].values.flatten().astype(int))))
-    if differences == 0:
-        perfect += 1
-correct = (correct / len(predictions)) * 100
-print("Correct: {}%".format(correct))
-perfect = (perfect / len(predictions)) * 100
-print("Perfect: {}%".format(perfect))
+#  csv_file = open("data/dataset.csv", "r")
+#
+#  data = pandas.read_csv("data/dataset.csv")
+#  print(data)
 
 
+def train_genres(data):
+    X = data.loc[
+        :,
+        (
+            "runtimeMinutes",
+            "numVotes",
+            "directorsExperience",
+            "actorsExperience",
+            "averageRating",
+        ),
+    ]
+    y = data.loc[
+        :,
+        (
+            "isAdult",
+            "isDrama",
+            "isComedy",
+            "isShort",
+            "isDocumentary",
+            "isTalkShow",
+            "isRomance",
+            "isFamily",
+            "isNews",
+            "isAnimation",
+            "isRealityTV",
+            "isMusic",
+            "isCrime",
+            "isAdventure",
+            "isGameShow",
+            "isMystery",
+            "isSport",
+            "isFantasy",
+            "isHorror",
+            "isThriller",
+            "isSciFi",
+            "isHistory",
+            "isBiography",
+            "isMusical",
+            "isWestern",
+            "isWar",
+            "isFilmNoir",
+        ),
+    ]
+    zipped = list(zip(X, y))
+    random.shuffle(zipped)
+    shuffled_X, shuffled_y = zip(*zipped)
 
-model = tree.DecisionTreeClassifier()
+    split = int(0.8 * len(X))
+    train_X, test_X = X[:split], X[split:]
+    train_y, test_y = y[:split], y[split:]
+
+    model = tree.DecisionTreeClassifier()
+
+    model.fit(train_X, train_y)
+    predictions = model.predict(test_X)
+    correct = 0.0
+    perfect = 0.0
+    for i, prediction in enumerate(predictions):
+        differences = sum(abs(prediction - test_y.loc[i + split]))
+        score = (len(prediction) - differences) / len(prediction)
+        correct += score
+        #  print(i)
+        #  print("Predict:\t{}".format(list(prediction.astype(int))))
+        #  print(
+            #  "Labels: \t{}".format(list(test_y.loc[i + split].values.flatten().astype(int)))
+        #  )
+        if differences == 0:
+            perfect += 1
+    correct = (correct / len(predictions))
+    #  print("Correct: {}%".format(correct))
+    perfect = (perfect / len(predictions))
+    #  print("Perfect: {}%".format(perfect))
+
+    return {"strengths": {"feature_d": 0.4, "feature_e": 0.3}, "accuracy": correct}
+
--- visualize.py
+++ visualize.py
@@ -6,9 +6,9 @@ import pandas
 import random
 
 import rating
+import genre
 
 plt.style.use("seaborn")
-
 
 
 csv_file = open("data/dataset.csv", "r")
@@ -18,7 +18,42 @@ sample_10k = data.sample(frac=1).reset_index(drop=True)
 sample_5k = data.sample(n=5000).reset_index(drop=True)
 sample_2k = data.sample(n=2000).reset_index(drop=True)
 
-
+genre_only = sample_10k.drop(
+    [
+        "runtimeMinutes",
+        "numVotes",
+        "directorsExperience",
+        "actorsExperience",
+        "averageRating",
+    ],
+    1,
+)
+genre_10 = sample_10k.drop(
+    [
+        "isRealityTV",
+        "isMusic",
+        "isCrime",
+        "isAdventure",
+        "isGameShow",
+        "isMystery",
+        "isSport",
+        "isFantasy",
+        "isHorror",
+        "isThriller",
+        "isSciFi",
+        "isHistory",
+        "isBiography",
+        "isMusical",
+        "isWestern",
+        "isWar",
+        "isFilmNoir",
+    ],
+    1,
+)
+genre_5 = genre_10.drop(
+    ["isTalkShow", "isRomance", "isFamily", "isNews", "isAnimation"], 1
+)
+genre_2 = genre_5.drop(["isComedy", "isShort", "isDocumentary"], 1)
 
 # Plots of predictor strength for rating against each feature
 plt.title("Rating Prediction Strength by Feature")
@@ -34,6 +69,41 @@ axes = plt.gca()
 axes.set_ylim([0, 1])
 
 plt.savefig("visuals/rating_predictor_strength.png", bbox_inches="tight")
+plt.clf()
+
+
+
+# Plots of predictor strength for genre against each feature
+plt.title("Genre Prediction Strength by Feature")
+plt.xlabel("Feature")
+plt.ylabel("Prediction Strength")
+
+result = genre.train_genres(sample_10k)["strengths"]
+
+for key, value in result.items():
+    plt.bar(key, value)
+
+axes = plt.gca()
+axes.set_ylim([0, 1])
+
+plt.savefig("visuals/genre_predictor_strength.png", bbox_inches="tight")
+plt.clf()
+
+
+
+# Plots of accuracy predicting rating with all (27), 10, 5, and 2 genres
+plt.title("Accuracy Predicting Rating by Quantity of Genres")
+plt.xlabel("Quantity of Genres")
+plt.ylabel("Accuracy")
+plt.bar("27", rating.train_rating(sample_10k)["accuracy"])
+plt.bar("10", rating.train_rating(genre_10)["accuracy"])
+plt.bar("5", rating.train_rating(genre_5)["accuracy"])
+plt.bar("2", rating.train_rating(genre_2)["accuracy"])
+
+axes = plt.gca()
+axes.set_ylim([0, 1])
+
+plt.savefig("visuals/rating_accuracy_quantity_genre.png", bbox_inches="tight")
 plt.clf()
 
 

0c6a34eb25134582769216b2bcb3a0797907f80f
NQureshi850 <nameer.qureshi@gmail.com>
2018-12-01 18:07:56 -0500

    should work now correctly

--- sources/validate.py
+++ sources/validate.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 import numpy as np
 import random
+import pandas
 
 # Input: number of folds k
 # numpy matrix X of features, with n rows (samples), d columns (features)
 # numpy vector y of scalar values, with n rows (samples), 1 column
 # Output: numpy vector z of scores of k rows, 1 column
 def kfoldscv(model,k,X,y):
-    
-    z = np.zeros((n,1))
+    #print("in")
+    X = X.values
+    X = np.asarray(X)
+    y = y.values
+    y = np.asarray(y)
+
+    z = np.zeros((k,1))
     combined = list(zip(X, y))
     random.shuffle(combined)
     X, y = zip(*combined)
 
     for i in range(k):
-        
+
         lower = ((len(X) * 1.0)/k) * i
         lower = np.floor(lower)
         lower = lower.astype(int)
@@ -23,24 +29,51 @@ def kfoldscv(model,k,X,y):
         upper = np.floor(upper)
         upper = upper.astype(int)
 
-        X_train = np.concatenate(X[:lower], X[upper:])
-        X_test = X[lower:upper]
+        lowerSamples = np.asarray(X[:lower])
+        upperSamples = np.asarray(X[upper:])
 
-        Y_train = np.concatenate(y[:lower], y[upper:])
-        Y_test = y[lower:upper]
+        #print(lowerSamples.shape)
+        #print(upperSamples.shape)
+
+        X_train = 0
+        if np.size(lowerSamples, 0) != 0 and np.size(upperSamples, 0) != 0:
+            X_train = np.concatenate((lowerSamples, upperSamples), axis=0)
+
+        if np.size(lowerSamples, 0) == 0:
+            X_train = upperSamples
+
+        if np.size(upperSamples, 0) == 0:
+            X_train = lowerSamples
+
+        X_test = np.asarray(X[lower:upper])
+
+        lowerSamples = np.asarray(y[:lower])
+        upperSamples = np.asarray(y[upper:])
+
+        Y_train = 0
+        if np.size(lowerSamples, 0) != 0 and np.size(upperSamples, 0) != 0:
+            Y_train = np.concatenate((lowerSamples, upperSamples), axis=0)
+
+        if np.size(lowerSamples, 0) == 0:
+            Y_train = upperSamples
+
+        if np.size(upperSamples, 0) == 0:
+            Y_train = lowerSamples
+
+        Y_test = np.asarray(y[lower:upper])
 
         model.fit(X_train, Y_train)
 
         predictions = model.predict(X_test)
-        print("predictions matrix: ")
-        print(predictions)
+        #print("predictions matrix: ")
+        #print(predictions)
 
         correct = 0.0
         for j, prediction in enumerate(predictions):
-            if prediction == y_test[j + i]:
+            if prediction.all() == Y_test[(j + i) - 1].all():
                 correct += 1
 
         z[i] = correct / len(predictions)
 
+    #print(z)
     return z
-
